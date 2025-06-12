231ee2423ba58bbff73d17e53647d2173f5540eb
algokelvin-373 <kelvinht373@gmail.com>
2025-06-12 10:33:06 +0700

    [Classification Man Women] Update Train

--- classification_man_women/train.py
+++ classification_man_women/train.py
@@ -43,6 +43,8 @@ def prepare_data():
         transforms.RandomRotation(15),
         transforms.ColorJitter(brightness=0.2, contrast=0.2),
         transforms.ToTensor(),
+        transforms.RandomAffine(degrees=10, translate=(0.1, 0.1)),
+        transforms.RandomPerspective(distortion_scale=0.2),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
